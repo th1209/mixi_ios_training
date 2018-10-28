8622e1a8361b84f5d1bb44c00a6bd7da9ee9b2e4
th1209 <hata.toshiki.1214@gmail.com>
2018-10-28 15:57:29 +0900

    3.1 UIViewの基礎(1)

---
+++ lecture/03.01/UiViewBasic/UiViewBasic/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UiViewBasic
+//
+//  Created by Hata_Toshiki on 2018/10/28.
+//  Copyright © 2018年 Hata_Toshiki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
